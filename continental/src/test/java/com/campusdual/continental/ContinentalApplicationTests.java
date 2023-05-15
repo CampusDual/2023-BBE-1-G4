c552e65c3d0572559af518a9750142ce47caa1e6
Lissz3 <Bd.isabel.rosado@gmail.com>
2023-05-15 14:40:35 +0200

    Spring project

---
+++ continental/src/test/java/com/campusdual/continental/ContinentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusdual.continental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
