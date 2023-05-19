a798b296550bc1a8d5f1b359c346e802b52e319d
AsierAsoreyPrado21 <aasoreyp@fpcoruna.afundacion.org>
2023-05-19 13:02:27 +0200

    CRUD CUSTOMER, API Requests insert customer & Test

---
+++ dominiondiamondhotel/src/main/java/com/campusdual/dominiondiamondhotel/api/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.campusdual.dominiondiamondhotel.api;
+
+import com.campusdual.dominiondiamondhotel.model.dto.CustomerDto;
+
+public interface ICustomerService {
+    int insertCustomer(CustomerDto customerDto);
+}
