790f3dc017c5f0674efac78fdbee6c299bc89cd2
Lissz3 <Bd.isabel.rosado@gmail.com>
2023-05-19 14:09:41 +0200

    Delete customer, API Request & Test

--- dominiondiamondhotel/src/main/java/com/campusdual/dominiondiamondhotel/api/ICustomerService.java
+++ dominiondiamondhotel/src/main/java/com/campusdual/dominiondiamondhotel/api/ICustomerService.java
@@ -1,7 +1,9 @@
 package com.campusdual.dominiondiamondhotel.api;
 
 import com.campusdual.dominiondiamondhotel.model.dto.CustomerDto;
+import org.springframework.http.ResponseEntity;
 
 public interface ICustomerService {
     int insertCustomer(CustomerDto customerDto);
+    ResponseEntity<?> deleteCustomer(CustomerDto customerDto);
 }
--- dominiondiamondhotel/src/main/java/com/campusdual/dominiondiamondhotel/service/CustomerService.java
+++ dominiondiamondhotel/src/main/java/com/campusdual/dominiondiamondhotel/service/CustomerService.java
@@ -7,6 +7,7 @@ import com.campusdual.dominiondiamondhotel.model.dto.CustomerDto;
 import com.campusdual.dominiondiamondhotel.model.dto.dtomapper.CustomerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service("CustomerService")
@@ -15,10 +16,22 @@ public class CustomerService implements ICustomerService {
 
     @Autowired
     private CustomerDao customerDao;
+
     @Override
     public int insertCustomer(CustomerDto customerDto) {
         Customer customer = CustomerMapper.INSTANCE.toEntity(customerDto);
         customerDao.saveAndFlush(customer);
         return customer.getId();
+    }
+
+    @Override
+    public ResponseEntity<?> deleteCustomer(CustomerDto customerDto) {
+        for (Customer c : customerDao.findAll()) {
+            if (c.getId() == CustomerMapper.INSTANCE.toEntity(customerDto).getId()) {
+                customerDao.delete(CustomerMapper.INSTANCE.toEntity(customerDto));
+                return ResponseEntity.ok(String.valueOf(c.getId()));
+            }
+        }
+        return ResponseEntity.badRequest().body("Customer not found.");
     }
 }
