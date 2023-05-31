80cb169d31a8288f3200a82fa1c215a26e7d7076
Lissz3 <Bd.isabel.rosado@gmail.com>
2023-05-31 10:40:19 +0200

    Validations

---
+++ dominiondiamondhotel/dominiondiamondhotel-model/src/main/java/com/ontimize/dominiondiamondhotel/model/core/dao/IdDocumentTypesDao.java
@@ -0,0 +1,17 @@
+package com.ontimize.dominiondiamondhotel.model.core.dao;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+@Repository(value = "IdDocumentTypesDao")
+@Lazy
+@ConfigurationFile(
+        configurationFile = "dao/IdDocumentTypesDao.xml",
+        configurationFilePlaceholder = "dao/placeholders.properties"
+)
+public class IdDocumentTypesDao extends OntimizeJdbcDaoSupport {
+    public static final String ATTR_ID = "id";
+    public static final String ATTR_IDTYPE = "idtype";
+}
--- dominiondiamondhotel/dominiondiamondhotel-model/src/main/java/com/ontimize/dominiondiamondhotel/model/core/service/CustomerService.java
+++ dominiondiamondhotel/dominiondiamondhotel-model/src/main/java/com/ontimize/dominiondiamondhotel/model/core/service/CustomerService.java
@@ -1,14 +1,19 @@
 package com.ontimize.dominiondiamondhotel.model.core.service;
 
 import com.ontimize.dominiondiamondhotel.api.core.service.ICustomerService;
+import com.ontimize.dominiondiamondhotel.api.core.utils.ValidatorUtils;
 import com.ontimize.dominiondiamondhotel.model.core.dao.CustomerDao;
+import com.ontimize.dominiondiamondhotel.model.core.dao.IdDocumentTypesDao;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +27,8 @@ public class CustomerService implements ICustomerService {
     @Autowired
     private CustomerDao customerDao;
 
+    @Autowired
+    private IdDocumentTypesDao idDocumentTypesDao;
 
     @Override
     public EntityResult customerQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException {
@@ -30,16 +37,57 @@ public class CustomerService implements ICustomerService {
 
     @Override
     public EntityResult customerInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
-        return this.daoHelper.insert(this.customerDao, attrMap);
+        String idNumber = String.valueOf(attrMap.get("idnumber"));
+        String phone = String.valueOf(attrMap.get("phone"));
+        int idTypeid = (int) attrMap.get("idtype_id");
+        Map<String, Object> idDocKeyMap = new HashMap<>();
+        idDocKeyMap.put(IdDocumentTypesDao.ATTR_ID, idTypeid);
+        List<String> idDocAttrList = new ArrayList<>();
+        idDocAttrList.add(IdDocumentTypesDao.ATTR_IDTYPE);
+        String email = String.valueOf(attrMap.get("mail"));
+        EntityResult typeIdExist = this.daoHelper.query(this.idDocumentTypesDao, idDocKeyMap, idDocAttrList);
+        Map<String, Object> customerKeyMap = new HashMap<>();
+        customerKeyMap.put(CustomerDao.ATTR_IDNUMBER, idNumber);
+        List<String> customerAttrList = new ArrayList<>();
+        customerAttrList.add(CustomerDao.ATTR_ID);
+        EntityResult idDocNumberAlreadyExist = this.daoHelper.query(this.customerDao, customerKeyMap, customerAttrList);
+        EntityResult er = new EntityResultMapImpl();
+        if (typeIdExist.get("idtype") != null &&
+                ValidatorUtils.idValidator(idTypeid, idNumber) &&
+                ValidatorUtils.emailValidator(email) &&
+                idDocNumberAlreadyExist.get("id") == null &&
+                ValidatorUtils.phoneValidator(phone)) {
+            return this.daoHelper.insert(this.customerDao, attrMap);
+        } else {
+            er.setMessage("Invalid data");
+        }
+        er.setCode(EntityResult.OPERATION_WRONG);
+        return er;
     }
 
     @Override
     public EntityResult customerUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap) throws OntimizeJEERuntimeException {
-        return this.daoHelper.update(this.customerDao, attrMap, keyMap);
+        String idNumber = String.valueOf(keyMap.get("idnumber"));
+        ArrayList<String> idtype = new ArrayList<>();
+        idtype.add(CustomerDao.ATTR_IDTYPE_ID.toLowerCase());
+        EntityResult entityResult = this.daoHelper.query(this.customerDao, keyMap, idtype);
+        int idTypeid = (int) ((List<?>) entityResult.get("idtype_id")).get(0);
+        String email = String.valueOf(attrMap.get("email"));
+        String phone = String.valueOf(attrMap.get("phone"));
+        EntityResult er = new EntityResultMapImpl();
+        if (ValidatorUtils.idValidator(idTypeid, idNumber) && ValidatorUtils.emailValidator(email) && ValidatorUtils.phoneValidator(phone)) {
+            return this.daoHelper.update(this.customerDao, attrMap, keyMap);
+        } else {
+            er.setMessage("Invalid data");
+        }
+        er.setCode(EntityResult.OPERATION_WRONG);
+        return er;
     }
 
     @Override
     public EntityResult customerDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
         return this.customerDao.delete(keyMap);
     }
+
+
 }
